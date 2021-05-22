ecab630a4da5b3e036a001ca2d84452a7e5d165a
mengelke <m.engelke@protonmail.com>
2021-05-22 18:27:26 +0200

    feat: buffer stack

--- sdks.c
+++ sdks.c
@@ -18,13 +18,20 @@
 
 /*
  * sdks.c
- * This file contains procedures to manipulate data in the sudoku strcture.
+ * This file contains procedures to manipulate data in the sudoku structure.
  */
 
 #include <stdlib.h>
+#include <string.h>
 
 #include "log.h"
 #include "sdks.h"
+
+static int sdks_stack_push(struct Sudoku *sdk);
+static void sdks_stack_pop(struct Sudoku *sdk);
+
+static struct Sudoku *sdkStack[SDK_STACK_SIZE];
+static int sdkStackPos = 0;
 
 /* Initialize Sudoku structure and return pointer to it.
  * Create and map arrays of pointers to provide a consistent abstracted interface
@@ -34,7 +41,7 @@
 struct Sudoku *sdks_init()
 {
 	int i, j, k, l;
-	struct Sudoku *sdk = malloc(sizeof(struct Sudoku));
+	struct Sudoku *sdk = (struct Sudoku*) malloc(sizeof(struct Sudoku));
 	sdk->freeCells = 0;
 	for (i = 0, j = 0; j < SDK_WIDTH; j++) {
 		for (k = 0; k < SDK_WIDTH; k++, i++) {
@@ -108,5 +115,37 @@ int sdks_solve(struct Sudoku *sdk)
 			continue;
 		}
 	}
-	return n;
+	LOG("%s: Done\n", mod);
+	return 1;
+}
+
+/* Copy sudoku structure onto the buffer stack.
+ * Return 0 if the stack is already full, non-zero otherwise.
+ */
+static int sdks_stack_push(struct Sudoku *sdk)
+{
+	#ifdef VERBOSE
+		char *mod = "sdks_stack_push";
+	#endif
+	if (sdkStackPos == SDK_STACK_SIZE - 1) {
+		LOG("%s: Error: Stack already at maximum size of %d!\n", mod, SDK_STACK_SIZE);
+		return 0;
+	}
+	LOG("%s: Copying sudoku to position %d of the buffer stack\n", mod, sdkStackPos);
+	sdkStack[sdkStackPos] = (struct Sudoku*) malloc(sizeof(struct Sudoku));
+	memcpy(sdkStack[sdkStackPos++], sdk, sizeof(struct Sudoku));
+	LOG("%s: Done\n", mod);
+	return 1;
+}
+
+/* Restore sudoku structure from the buffer stack copy and the memory used by the latter. */
+static void sdks_stack_pop(struct Sudoku *sdk)
+{
+	#ifdef VERBOSE
+		char *mod = "sdks_stack_push";
+	#endif
+	LOG("%s: Restoring sudoku from position %d of the buffer stack\n", mod, sdkStackPos);
+	memcpy(sdk, sdkStack[sdkStackPos], sizeof(struct Sudoku));
+	free(sdkStack[sdkStackPos--]);
+	LOG("%s: Done\n", mod);
 }
